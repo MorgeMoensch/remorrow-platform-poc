ad03f3aa31ad38b208b5bda6ebe92158c6d9722f
MorgeMoensch <morgemoensch@morgemoensch.dev>
2023-12-23 17:14:17 +0100

    Basic Fetch from Supabase

---
+++ app/routes/projects.tsx
@@ -0,0 +1,17 @@
+import type { ActionFunctionArgs } from '@remix-run/node';
+import {useLoaderData} from "@remix-run/react";
+import {createSupabaseServerClient} from "../supabase.server";
+
+export const loader = async ({request}:ActionFunctionArgs) => {
+
+    const { supabaseClient, headers } = createSupabaseServerClient(request)
+
+    const data = await supabaseClient.from("projects").select();
+
+    return {data};
+};
+
+export default function Projects() {
+    const data = useLoaderData();
+    return <pre>{JSON.stringify(data, null, 2)}</pre>
+}
