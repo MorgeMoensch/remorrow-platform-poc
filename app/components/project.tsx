550d838d32b11216a43b74f61b8e7a9cf4974778
MorgeMoensch <morgemoensch@morgemoensch.dev>
2024-01-13 14:36:40 +0100

    Use proper fit for project-images and use pseudo-random images on projects instead of placeholder.

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -1,9 +1,23 @@
 export default function Project({title}) {
+
+    const getRandomImage = () => {
+        const imageUrls = [
+            "https://images.unsplash.com/photo-1453614512568-c4024d13c247?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1558449028-b53a39d100fc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1608387371413-f2566ac510e0?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1681140560555-2a054c898b66?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1610093703375-6d8fd641294b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"];
+
+        const randomImageIndex = Math.floor(Math.random() * imageUrls.length)
+
+        return imageUrls[randomImageIndex];
+    }
+
     return <>
     <div className={"project-card"}>
         <img
             style={{width: "15em"}}
-            src={"https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"} />
+            src={getRandomImage()} />
         <h2 className={"font-semibold text-gray-900"}>{title}</h2>
     </div></>
 }
