887933a33adbdb5185396994fe9ea39208b14043
MorgeMoensch <morgemoensch@morgemoensch.dev>
2024-02-04 15:57:58 +0100

    Add Button on Projects overview to add new Project

--- app/routes/projects._index.tsx
+++ app/routes/projects._index.tsx
@@ -1,5 +1,5 @@
 import type { ActionFunctionArgs } from '@remix-run/node';
-import {useLoaderData} from "@remix-run/react";
+import {useLoaderData, Link} from "@remix-run/react";
 import {createSupabaseServerClient} from "../supabase.server";
 import ProjectOverviewCard from "~/components/ProjectOverviewCard";
 
@@ -35,14 +35,18 @@ export default function Projects() {
     return <>
         <img className={"header-image"} src={"https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
         <section>
-            <h1 className="text-3xl font-bold" style={{marginBottom: "2rem"}}>
-                Projects
-            </h1>
-            <div id={"project-overview-wrapper"}>
-                {projectElements}
+            <div style={{maxWidth: "95vw"}}>
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                    <h1 className="text-3xl font-bold" style={{marginBottom: "2rem"}}>
+                        Projects
+                    </h1>
+                    <Link to="add" style={{height: "2.5rem"}} className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add your project</Link>
+                </div>
+                <div id={"project-overview-wrapper"}>
+                    {projectElements}
 
+                </div>
             </div>
-
         </section>
 
         <pre>{JSON.stringify(data, null, 2)}</pre>
