230f172040e66c2d7255de8b5fd222751958dcdc
MorgeMoensch <morgemoensch@morgemoensch.dev>
2024-05-09 11:02:58 +0200

    Update Project Registering with fewer fields

--- app/routes/projects.add.tsx
+++ app/routes/projects.add.tsx
@@ -11,33 +11,66 @@ export default function Add() {
 
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div className="sm:col-span-4">
-                            <label for="title" className="block text-sm font-medium leading-6 text-gray-900">Title *</label>
+                            <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">Title
+                                *</label>
                             <div className="mt-2">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <input type="text" name="title" id="title" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Energiesparlampen für Siriks Lifecoaching" />
+                                <div
+                                    className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                    <input type="text" name="title" id="title"
+                                           className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                           placeholder="Energiesparlampen für Siriks Lifecoaching"/>
                                 </div>
                             </div>
                         </div>
 
                         <div className="col-span-full">
-                            <label for="description" className="block text-sm font-medium leading-6 text-gray-900">Description</label>
+                            <label htmlFor="description"
+                                   className="block text-sm font-medium leading-6 text-gray-900">Description *</label>
                             <div className="mt-2">
-                                <textarea id="description" name="description" rows="3" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                <textarea id="description" name="description" rows="3"
+                                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                             </div>
-                            <p className="mt-3 text-sm leading-6 text-gray-600">Explain what your project is about and your motivation behind it. How does it contribute to a better tomorrow?</p>
+                            <p className="mt-3 text-sm leading-6 text-gray-600">Explain what your project is about and
+                                your motivation behind it. How does it contribute to a better tomorrow?</p>
                         </div>
 
                         <div className="col-span-full">
-                            <label for="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
-                            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                            <label htmlFor="support-needs"
+                                   className="block text-sm font-medium leading-6 text-gray-900">Wo brauchst du Unterstützung? *</label>
+                            <div className="mt-2">
+                                <textarea id="support-needs" name="support-needs" rows="3"
+                                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                            </div>
+                            <p className="mt-3 text-sm leading-6 text-gray-600">Wie können dir andere Leute oder Unternehmen helfen?</p>
+                        </div>
+
+                        <div className="col-span-full">
+                            <label htmlFor="facts"
+                                   className="block text-sm font-medium leading-6 text-gray-900">Eckdaten</label>
+                            <div className="mt-2">
+                                <textarea id="facts" name="facts" rows="3"
+                                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                            </div>
+                            <p className="mt-3 text-sm leading-6 text-gray-600">Ort, Datum, Dauer, Entschädigung, ...</p>
+                        </div>
+
+                        <div className="col-span-full">
+                            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">Cover
+                                photo</label>
+                            <div
+                                className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                                 <div className="text-center">
-                                    <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+                                    <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24"
+                                         fill="currentColor" aria-hidden="true">
+                                        <path fill-rule="evenodd"
+                                              d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                                              clip-rule="evenodd"/>
                                     </svg>
                                     <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                                        <label for="file-upload" className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                        <label htmlFor="file-upload"
+                                               className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                                             <span>Upload a file</span>
-                                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                            <input id="file-upload" name="file-upload" type="file" className="sr-only"/>
                                         </label>
                                         <p className="pl-1">or drag and drop</p>
                                     </div>
@@ -48,74 +81,6 @@ export default function Add() {
                     </div>
                 </div>
 
-                <div className="border-b border-gray-900/10 pb-12">
-                    <h2 className="text-base font-semibold leading-7 text-gray-900">Detailed informations</h2>
-                    <p className="mt-1 text-sm leading-6 text-gray-600">To enable filtering by category and to suggest matching partners, we require some more information about your project.</p>
-
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div className="sm:col-span-3">
-                            <label htmlFor="date"
-                                   className="block text-sm font-medium leading-6 text-gray-900">Datum</label>
-                            <div className="mt-2">
-                                <input type="text" name="date" id="date"
-                                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-3">
-                            <label htmlFor="place"
-                                   className="block text-sm font-medium leading-6 text-gray-900">Ort</label>
-                            <div className="mt-2">
-                                <input id="place" name="place" type="text"
-                                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-3">
-                            <label htmlFor="compensation"
-                                   className="block text-sm font-medium leading-6 text-gray-900">Compensation</label>
-                            <div className="mt-2">
-                                <input id="compensation" name="compensation" type="text"
-                                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-                            </div>
-                        </div>
-                        <div className="col-span-full">
-                            <label htmlFor="requirements"
-                                   className="block text-sm font-medium leading-6 text-gray-900">Requirements</label>
-                            <div className="mt-2">
-                                <input id="requirements" name="requirements" type="text"
-                                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-                            </div>
-                        </div>
-
-                        <div className="sm:col-span-3">
-                            <label htmlFor="category"
-                                   className="block text-sm font-medium leading-6 text-gray-900">Category</label>
-                            <div className="mt-2">
-                                <select id="category" name="category"
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                    <option>Food</option>
-                                    <option>Energy</option>
-                                    <option>Volunteering</option>
-                                    <option>Digital</option>
-                                    <option>Nature</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div className="col-span-full">
-                            <div className="flex h-6 items-center">
-                                <input id="certificate" name="certificate" type="checkbox"
-                                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
-                            </div>
-                            <div className="text-sm leading-6">
-                                <label for="certificate"
-                                       className="font-medium text-gray-900">Certificate</label>
-                                <p className="text-gray-500">I want my project to be certified by re:morrow (200.-)</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <div className="mt-6 flex items-center justify-end gap-x-6">
                     <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
